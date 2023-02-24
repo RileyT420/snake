604195d594165bb81216ce8e8ae7b9fd470e2d67
Riley Tate <124rtate@frhsd.com>
2023-02-24 10:52:35 -0500

    .

--- js/snake.js
+++ js/snake.js
@@ -18,7 +18,7 @@ export function update() {
 
 export function draw(gameBoard) {
   snakeBody.forEach(segment => {
-    const snakeElement = document.createElement('div')
+    var snakeElement = document.createElement('div')
     snakeElement.style.gridRowStart = segment.y
     snakeElement.style.gridColumnStart = segment.x
     snakeElement.classList.add('snake')
