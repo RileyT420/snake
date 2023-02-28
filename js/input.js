756ddf2076cfb3f4f714a93c3713c37ab24d534f
Riley Tate <124rtate@frhsd.com>
2023-02-28 08:33:40 -0500

    .

--- js/input.js
+++ js/input.js
@@ -1,23 +1,33 @@
 let inputDirection = { x: 0, y: 0 }
 let lastInputDirection = { x: 0, y: 0 }
+const snake = document.getElementsByClassName('snake');
+let prevRotate = 0;
 
 window.addEventListener('keydown', e => {
   switch (e.key) {
     case 'ArrowUp':
       if (lastInputDirection.y !== 0) break
       inputDirection = { x: 0, y: -1 }
+      snake.style.transform = 'rotate(-'+ prevRotate + ')';
+      snake.style.transform = 'rotate(270deg)';
+      prevRotate = 270;
       break
     case 'ArrowDown':
       if (lastInputDirection.y !== 0) break
       inputDirection = { x: 0, y: 1 }
+      snake.style.transform = 'rotate(-'+ prevRotate + ')';
+      snake.style.transform = 'rotate(90deg)';
+      prevRotate = 90;
       break
     case 'ArrowLeft':
       if (lastInputDirection.x !== 0) break
       inputDirection = { x: -1, y: 0 }
+      snake.style.transform = 'rotate(180deg)';
       break
     case 'ArrowRight':
       if (lastInputDirection.x !== 0) break
       inputDirection = { x: 1, y: 0 }
+      snake.style.transform = 'rotate(0deg)';
       break
   }
 })
